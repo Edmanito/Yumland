41fa65df0898360dda2319ae5df8fb37ba1c8b87
Edmanito <168591861+Edmanito@users.noreply.github.com>
2026-02-11 11:49:00 +0100

    push fin de séance 11/02

--- zeus_shunei.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title ALERTE SYSTEME KAISEKI
-echo PAR LE TONNERRE DE ZEUS, TU AS TROP JOUE.
-echo Fermeture du systeme dans 3 secondes...
-timeout /t 3
-C:\Windows\System32\shutdown.exe /s /f /t 0
